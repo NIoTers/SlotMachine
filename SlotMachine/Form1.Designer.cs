7054ccc277054d83b3c19a8bb8f4251702026ae8
kuukolol <alarcon.321748@cubao.sti.edu.ph>
2025-03-18 01:07:19 +0800

    Add project files.

---
+++ SlotMachine/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.background = new System.Windows.Forms.PictureBox();
+            this.slot1 = new System.Windows.Forms.PictureBox();
+            this.slot2 = new System.Windows.Forms.PictureBox();
+            this.slot3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddBalance = new System.Windows.Forms.Button();
+            this.Spin = new System.Windows.Forms.Button();
+            this.Bet = new System.Windows.Forms.Button();
+            this.money = new System.Windows.Forms.Label();
+            this.BetAmount = new System.Windows.Forms.Label();
+            this.betTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.Image = ((System.Drawing.Image)(resources.GetObject("background.Image")));
+            this.background.Location = new System.Drawing.Point(206, 12);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(557, 378);
+            this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            // 
+            // slot1
+            // 
+            this.slot1.Location = new System.Drawing.Point(267, 212);
+            this.slot1.Name = "slot1";
+            this.slot1.Size = new System.Drawing.Size(133, 131);
+            this.slot1.TabIndex = 1;
+            this.slot1.TabStop = false;
+            // 
+            // slot2
+            // 
+            this.slot2.Location = new System.Drawing.Point(430, 212);
+            this.slot2.Name = "slot2";
+            this.slot2.Size = new System.Drawing.Size(132, 131);
+            this.slot2.TabIndex = 2;
+            this.slot2.TabStop = false;
+            // 
+            // slot3
+            // 
+            this.slot3.Location = new System.Drawing.Point(592, 212);
+            this.slot3.Name = "slot3";
+            this.slot3.Size = new System.Drawing.Size(132, 131);
+            this.slot3.TabIndex = 3;
+            this.slot3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(69)))), ((int)(((byte)(68)))));
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(238, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Balance:";
+            // 
+            // AddBalance
+            // 
+            this.AddBalance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(165)))), ((int)(((byte)(0)))));
+            this.AddBalance.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddBalance.FlatAppearance.BorderSize = 0;
+            this.AddBalance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddBalance.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.AddBalance.ForeColor = System.Drawing.Color.White;
+            this.AddBalance.Location = new System.Drawing.Point(260, 396);
+            this.AddBalance.Name = "AddBalance";
+            this.AddBalance.Size = new System.Drawing.Size(140, 45);
+            this.AddBalance.TabIndex = 5;
+            this.AddBalance.Text = "➕ Add Balance";
+            this.AddBalance.UseVisualStyleBackColor = false;
+            this.AddBalance.Click += new System.EventHandler(this.AddBalance_Click);
+            // 
+            // Spin
+            // 
+            this.Spin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(165)))), ((int)(((byte)(0)))));
+            this.Spin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Spin.FlatAppearance.BorderSize = 0;
+            this.Spin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Spin.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.Spin.ForeColor = System.Drawing.Color.White;
+            this.Spin.Location = new System.Drawing.Point(422, 396);
+            this.Spin.Name = "Spin";
+            this.Spin.Size = new System.Drawing.Size(140, 45);
+            this.Spin.TabIndex = 6;
+            this.Spin.Text = "🎰 Spin";
+            this.Spin.UseVisualStyleBackColor = false;
+            // 
+            // Bet
+            // 
+            this.Bet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
+            this.Bet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Bet.FlatAppearance.BorderSize = 0;
+            this.Bet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bet.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.Bet.ForeColor = System.Drawing.Color.White;
+            this.Bet.Location = new System.Drawing.Point(592, 396);
+            this.Bet.Name = "Bet";
+            this.Bet.Size = new System.Drawing.Size(140, 45);
+            this.Bet.TabIndex = 7;
+            this.Bet.Text = "💵 Bet";
+            this.Bet.UseVisualStyleBackColor = false;
+            this.Bet.Click += new System.EventHandler(this.Bet_Click);
+            // 
+            // money
+            // 
+            this.money.AutoSize = true;
+            this.money.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(69)))), ((int)(((byte)(68)))));
+            this.money.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.money.ForeColor = System.Drawing.Color.Black;
+            this.money.Location = new System.Drawing.Point(343, 354);
+            this.money.Name = "money";
+            this.money.Size = new System.Drawing.Size(24, 27);
+            this.money.TabIndex = 4;
+            this.money.Text = "0";
+            // 
+            // BetAmount
+            // 
+            this.BetAmount.AutoSize = true;
+            this.BetAmount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(69)))), ((int)(((byte)(68)))));
+            this.BetAmount.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BetAmount.ForeColor = System.Drawing.Color.Black;
+            this.BetAmount.Location = new System.Drawing.Point(587, 354);
+            this.BetAmount.Name = "BetAmount";
+            this.BetAmount.Size = new System.Drawing.Size(58, 27);
+            this.BetAmount.TabIndex = 8;
+            this.BetAmount.Text = "Bet:";
+            // 
+            // betTotal
+            // 
+            this.betTotal.AutoSize = true;
+            this.betTotal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(69)))), ((int)(((byte)(68)))));
+            this.betTotal.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.betTotal.ForeColor = System.Drawing.Color.Black;
+            this.betTotal.Location = new System.Drawing.Point(643, 355);
+            this.betTotal.Name = "betTotal";
+            this.betTotal.Size = new System.Drawing.Size(24, 27);
+            this.betTotal.TabIndex = 9;
+            this.betTotal.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(932, 482);
+            this.Controls.Add(this.betTotal);
+            this.Controls.Add(this.BetAmount);
+            this.Controls.Add(this.money);
+            this.Controls.Add(this.Bet);
+            this.Controls.Add(this.Spin);
+            this.Controls.Add(this.AddBalance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.slot3);
+            this.Controls.Add(this.slot2);
+            this.Controls.Add(this.slot1);
+            this.Controls.Add(this.background);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Slot Machine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox background;
+        private PictureBox slot1;
+        private PictureBox slot2;
+        private PictureBox slot3;
+        private Label label1;
+        private Button AddBalance;
+        private Button Spin;
+        private Button Bet;
+        private Label money;
+        private Label BetAmount;
+        private Label betTotal;
+    }
+}
+
---
+++ SlotMachine/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        int currentBalance = 0;
+        int betAmount = 0;
+        Random rng = new Random();
+        Timer spinTimer = new Timer();
+        int spinDuration = 3000;
+        DateTime spinStartTime;
+        string[] imageNames = { "bar.png", "bell.png", "cherry.png", "diamond.png", "lemon.png", "seven.png" };
+        string imagePath = Path.Combine(Directory.GetParent(Application.StartupPath).Parent.FullName, "Images\\");
+        string[] slotResults = new string[3];
+
+        public Form1()
+        {
+            InitializeComponent();
+            ApplyRoundedCorners(AddBalance, 10);
+            ApplyRoundedCorners(Spin, 10);
+            ApplyRoundedCorners(Bet, 10);
+            this.AddBalance.MouseEnter += (s, e) => { AddBalance.BackColor = ColorTranslator.FromHtml("#FF8C00"); };
+            this.AddBalance.MouseLeave += (s, e) => { AddBalance.BackColor = ColorTranslator.FromHtml("#FFA500"); };
+            this.Spin.MouseEnter += (s, e) => { Spin.BackColor = ColorTranslator.FromHtml("#FF8C00"); };
+            this.Spin.MouseLeave += (s, e) => { Spin.BackColor = ColorTranslator.FromHtml("#FFA500"); };
+            this.Bet.MouseEnter += (s, e) => { Bet.BackColor = ColorTranslator.FromHtml("#0056b3"); };
+            this.Bet.MouseLeave += (s, e) => { Bet.BackColor = ColorTranslator.FromHtml("#007BFF"); };
+            spinTimer.Interval = 100;
+            spinTimer.Tick += SpinTimer_Tick;
+            this.Spin.Click += Spin_Click;
+        }
+
+        private void ApplyRoundedCorners(Button btn, int radius)
+        {
+            Rectangle bounds = btn.ClientRectangle;
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(bounds.X, bounds.Y, radius, radius, 180, 90);
+            path.AddArc(bounds.Right - radius, bounds.Y, radius, radius, 270, 90);
+            path.AddArc(bounds.Right - radius, bounds.Bottom - radius, radius, radius, 0, 90);
+            path.AddArc(bounds.X, bounds.Bottom - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+            btn.Region = new Region(path);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void AddBalance_Click(object sender, EventArgs e)
+        {
+            string input = Interaction.InputBox("Enter amount to add:", "Add Balance", "100", -1, -1);
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                if (int.TryParse(input, out int amount) && amount > 0)
+                {
+                    MessageBox.Show($"You added {amount} money!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    currentBalance += amount;
+                    money.Text = currentBalance.ToString("N0");
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid positive number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void Bet_Click(object sender, EventArgs e)
+        {
+            string input = Interaction.InputBox("Enter amount to bet:", "Bet", "100", -1, -1);
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                if (int.TryParse(input, out int amount) && amount > 0)
+                {
+                    if (amount > currentBalance)
+                    {
+                        MessageBox.Show("You cannot bet more than your current balance.", "Not Enough Balance", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    betAmount = amount;
+                    betTotal.Text = betAmount.ToString("N0");
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid positive number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void Spin_Click(object sender, EventArgs e)
+        {
+            if (betAmount == 0)
+            {
+                MessageBox.Show("Please add a bet amount first", "No Bet Found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (currentBalance <= 0)
+            {
+                MessageBox.Show("You don't have enough balance to place a bet.", "Insufficient Balance", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (betAmount > currentBalance && currentBalance > 0)
+            {
+                MessageBox.Show("You cannot bet more than your current balance.", "Not Enough Balance", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+           
+
+            spinStartTime = DateTime.Now;
+            spinTimer.Start();
+        }
+
+
+
+        private void SpinTimer_Tick(object sender, EventArgs e)
+        {
+            slot1.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot2.SizeMode = PictureBoxSizeMode.StretchImage;
+            slot3.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            try
+            {
+                slotResults[0] = imageNames[rng.Next(imageNames.Length)];
+                slotResults[1] = imageNames[rng.Next(imageNames.Length)];
+                slotResults[2] = imageNames[rng.Next(imageNames.Length)];
+
+                slot1.Image = Image.FromFile(imagePath + slotResults[0]);
+                slot2.Image = Image.FromFile(imagePath + slotResults[1]);
+                slot3.Image = Image.FromFile(imagePath + slotResults[2]);
+            }
+            catch (Exception ex)
+            {
+                spinTimer.Stop();
+                MessageBox.Show("Error loading images: " + ex.Message);
+            }
+
+            if ((DateTime.Now - spinStartTime).TotalMilliseconds >= spinDuration)
+            {
+                spinTimer.Stop();
+                CheckWin();
+            }
+        }
+
+        private void CheckWin()
+        {
+            if (slotResults[0] == slotResults[1] && slotResults[1] == slotResults[2])
+            {
+                if (slotResults[0] == "seven.png")
+                {
+                    currentBalance += betAmount * 2;
+                    MessageBox.Show($"You won double! Current balance: {currentBalance}", "Win", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    currentBalance += betAmount;
+                    MessageBox.Show($"You won the full bet! Current balance: {currentBalance}", "Win", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else if (slotResults[0] == slotResults[1] || slotResults[1] == slotResults[2] || slotResults[0] == slotResults[2])
+            {
+                currentBalance += betAmount / 2; 
+                MessageBox.Show($"You won half of the bet! Current balance: {currentBalance}", "Win", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                currentBalance -= betAmount;
+                MessageBox.Show($"You lost the bet! Current balance: {currentBalance}", "Loss", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            money.Text = currentBalance.ToString("N0");
+        }
+
+    }
+}
